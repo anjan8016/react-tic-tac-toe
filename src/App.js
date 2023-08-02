bc07c7cb99f0797f4459120e988bcc1580a0cdf8
anjan8016 <anjan801675@gmail.com>
2023-08-02 22:15:56 +0530

    Delete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import Board from './components/Board'
-import './App.css';
-import React, { useState , createContext, useEffect } from 'react';
-import Header from './components/Header';
+import Board from "./components/Board";
+import "./App.css";
+import React, { useState, createContext, useEffect } from "react";
+import Header from "./components/Header";
 
 export const AppContext = createContext();
 
 function App() {
-  const emptyGame = [["", "", ""], ["", "", ""], ["", "", ""]]
-  const [cells, setCells] = useState(emptyGame)
-  const [winnerCells, setWinnerCells] = useState([[],[],[]])
+  const emptyGame = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  const [cells, setCells] = useState(emptyGame);
+  const [winnerCells, setWinnerCells] = useState([[], [], []]);
 
-  const X = "X"
-  const O = "O"
+  const X = "X";
+  const O = "O";
   const [currentChar, setCurrentChar] = useState(X);
   const [winner, setWinner] = useState("");
-  const [gameOver, setGameOver] = useState(false)
+  const [gameOver, setGameOver] = useState(false);
 
-  useEffect(function() {
-    isGameOver()
-  }, [cells])
+  useEffect(
+    function () {
+      isGameOver();
+    },
+    [cells]
+  );
 
   function cellClick(row, column) {
     if (gameOver || winner) {
@@ -29,108 +36,148 @@ function App() {
       return;
     }
 
-    const newBoard = {...cells}
-    newBoard[row][column] = currentChar
-    setCells(newBoard)
-    changeChar()
+    const newBoard = { ...cells };
+    newBoard[row][column] = currentChar;
+    setCells(newBoard);
+    changeChar();
   }
 
   function changeChar() {
     if (currentChar == X) {
-      setCurrentChar(O)
+      setCurrentChar(O);
     } else {
-      setCurrentChar(X)
+      setCurrentChar(X);
     }
   }
 
   function reset() {
-    setCells(emptyGame)
-    setWinner("")
-    setGameOver(false)
-    setWinnerCells([[],[],[]])
+    setCells(emptyGame);
+    setWinner("");
+    setGameOver(false);
+    setWinnerCells([[], [], []]);
   }
 
   function isGameOver() {
     switch (true) {
-      case areTheSameInRow(0): return
-      case areTheSameInRow(1): return
-      case areTheSameInRow(2): return
-      case areTheSameInColumn(0): return
-      case areTheSameInColumn(1): return
-      case areTheSameInColumn(2): return
-      case areTheSameInDiagonal(): return
+      case areTheSameInRow(0):
+        return;
+      case areTheSameInRow(1):
+        return;
+      case areTheSameInRow(2):
+        return;
+      case areTheSameInColumn(0):
+        return;
+      case areTheSameInColumn(1):
+        return;
+      case areTheSameInColumn(2):
+        return;
+      case areTheSameInDiagonal():
+        return;
     }
 
-    if (!cells[0].includes("") && !cells[1].includes("") && !cells[2].includes("")) {
-      endGame("")
+    if (
+      !cells[0].includes("") &&
+      !cells[1].includes("") &&
+      !cells[2].includes("")
+    ) {
+      endGame("");
     }
   }
 
   function endGame(winner) {
-    if (winner != "") setWinner(winner)
-    setGameOver(true)
+    if (winner != "") setWinner(winner);
+    setGameOver(true);
   }
 
   function areTheSameInRow(row) {
-    if (cells[row][0] !== "" && cells[row][0] === cells[row][1] && cells[row][0] === cells[row][2]) {
-      endGame(cells[row][0])
-      
-      const newWinner = [[],[],[]]
-      newWinner[row][0] = true
-      newWinner[row][1] = true
-      newWinner[row][2] = true
-      setWinnerCells(newWinner)
-      return true
+    if (
+      cells[row][0] !== "" &&
+      cells[row][0] === cells[row][1] &&
+      cells[row][0] === cells[row][2]
+    ) {
+      endGame(cells[row][0]);
+
+      const newWinner = [[], [], []];
+      newWinner[row][0] = true;
+      newWinner[row][1] = true;
+      newWinner[row][2] = true;
+      setWinnerCells(newWinner);
+      return true;
     }
-    return false
+    return false;
   }
 
   function areTheSameInColumn(column) {
-    if (cells[0][column] !== "" && cells[0][column] === cells[1][column] && cells[2][column] == cells[0][column]) {
-      endGame(cells[0][column])
-      
-      const newWinner = [[],[],[]]
-      newWinner[0][column] = true
-      newWinner[1][column] = true
-      newWinner[2][column] = true
-      setWinnerCells(newWinner)
-      return true
+    if (
+      cells[0][column] !== "" &&
+      cells[0][column] === cells[1][column] &&
+      cells[2][column] == cells[0][column]
+    ) {
+      endGame(cells[0][column]);
+
+      const newWinner = [[], [], []];
+      newWinner[0][column] = true;
+      newWinner[1][column] = true;
+      newWinner[2][column] = true;
+      setWinnerCells(newWinner);
+      return true;
     }
-    return false
+    return false;
   }
 
   function areTheSameInDiagonal() {
-    if (cells[1][1] != "" && (cells[0][0] === cells[1][1] && cells[0][0] == cells[2][2])) {
-      endGame(cells[1][1])
-      const newWinner = [[],[],[]]
-      newWinner[0][0] = true
-      newWinner[1][1] = true
-      newWinner[2][2] = true
-      setWinnerCells(newWinner)
-      return true
+    if (
+      cells[1][1] != "" &&
+      cells[0][0] === cells[1][1] &&
+      cells[0][0] == cells[2][2]
+    ) {
+      endGame(cells[1][1]);
+      const newWinner = [[], [], []];
+      newWinner[0][0] = true;
+      newWinner[1][1] = true;
+      newWinner[2][2] = true;
+      setWinnerCells(newWinner);
+      return true;
     }
 
-    if (cells[1][1] != "" && (cells[2][0] === cells[1][1] && cells[2][0] == cells[0][2])) {
-      endGame(cells[1][1])
-      const newWinner = [[],[],[]]
-      newWinner[0][2] = true
-      newWinner[1][1] = true
-      newWinner[2][0] = true
-      setWinnerCells(newWinner)
-      return true
+    if (
+      cells[1][1] != "" &&
+      cells[2][0] === cells[1][1] &&
+      cells[2][0] == cells[0][2]
+    ) {
+      endGame(cells[1][1]);
+      const newWinner = [[], [], []];
+      newWinner[0][2] = true;
+      newWinner[1][1] = true;
+      newWinner[2][0] = true;
+      setWinnerCells(newWinner);
+      return true;
     }
-    return false
+    return false;
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <AppContext.Provider value={{cells, setCells, cellClick, currentChar, winner, gameOver, winnerCells}}>
+        <AppContext.Provider
+          value={{
+            cells,
+            setCells,
+            cellClick,
+            currentChar,
+            winner,
+            gameOver,
+            winnerCells,
+          }}
+        >
+          <img src="Toton.jpg" height="200" width="200" alt="toton" />
           <Header />
           <Board />
         </AppContext.Provider>
 
-        <button className='btn-reset' onClick={() => reset()}>Reset</button>
+        <button className="btn-reset" onClick={() => reset()}>
+          Reset
+        </button>
       </header>
     </div>
   );
